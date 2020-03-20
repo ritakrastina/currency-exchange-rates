fd270f5a313d4366194f4cba4012ccca931e5019
ritakrastina <62397874+ritakrastina@users.noreply.github.com>
2020-03-20 14:13:07 +0200

    Fix project name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('exchange-rates app is running!');
+    expect(page.getTitleText()).toEqual('currency-exchange-rates app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/exchange-rates'),
+      dir: require('path').join(__dirname, './coverage/currency-exchange-rates'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
