1090cd42805fe5dc97c2c090acb1a3ef60c589c0
ritakrastina <62397874+ritakrastina@users.noreply.github.com>
2020-03-20 14:24:29 +0200

    Remove unnecessary files

--- src/app/pages/all-rates/all-rates-filter.ts
+++
@@ -1,6 +0,0 @@
-import { Currency } from '../../currency.enum';
-
-export interface AllRatesFilter {
-  baseCurrency: Currency,
-  date: Date,
-}
--- src/app/pages/single-rate/single-rate-filter.ts
+++
@@ -1,8 +0,0 @@
-import { Currency } from '../../currency.enum';
-
-export interface SingleRateFilter {
-  selectedCurrency: Currency,
-  baseCurrency: Currency,
-  dateFrom: Date,
-  dateTill: Date,
-}
